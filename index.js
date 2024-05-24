5fa64c8b327bef80e570ae4691a5e834a0951670
MateusMacielQuatrochi` <mateusmquatrochi@hotmail.com`>
2024-05-23 21:27:37 -0300

    Alteraçoes 6

--- index.js
+++ index.js
@@ -249,7 +249,10 @@ function cadastrarUsuario(requisicao, resposta){
                             </div>`);
         }
         
-        resposta.write(` <input type="submit" value="Enviar">
+        resposta.write(` <div class="col-12 mb-3">
+        <button class="btn btn-primary" type="submit">Cadastrar</button>
+        <a class="btn btn-secondary" href="/">Voltar</a>                   
+    </div>
         </form>
     </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
@@ -274,6 +277,71 @@ app.get('/listarUsuarios', (req,resp)=>{
     resp.write('<meta charset="utf-8">');
     resp.write('</head>');
     resp.write('<body>');
+    resp.write(`<script>`);
+    resp.write(`.table {
+        width: 100%;
+        border-collapse: collapse;
+        margin: 20px 0;
+      }
+      
+      .table th, .table td {
+        border: 1px solid #ddd;
+        padding: 10px;
+        text-align: left;
+      }
+      
+      .table th {
+        background-color: #f0f0f0;
+      }
+      
+      .table-striped tr:nth-child(even) {
+        background-color: #f9f9f9;
+      }
+      
+      .table-striped tr:nth-child(odd) {
+        background-color: #fff;
+      }
+      
+      .table td {
+        vertical-align: top;
+      }
+      
+      .table td:nth-child(1) {
+        width: 15%;
+      }
+      
+      .table td:nth-child(2) {
+        width: 25%;
+      }
+      
+      .table td:nth-child(3) {
+        width: 20%;
+      }
+      
+      .table td:nth-child(4) {
+        width: 20%;
+      }
+      
+      .table td:nth-child(5) {
+        width: 10%;
+      }
+      
+      .table td:nth-child(6) {
+        width: 10%;
+      }
+      
+      .table td:nth-child(7) {
+        width: 10%;
+      }
+      
+      .table td:nth-child(8) {
+        width: 20%;
+      }
+      
+      .table td:nth-child(9) {
+        width: 15%;
+      }`);
+      response.write(`</script`);
     resp.write('<h1>Lista de Usuários</h1>');
     resp.write('<table class="table table-striped">');
     resp.write('<tr>');
